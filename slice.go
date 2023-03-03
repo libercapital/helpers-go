1ca3570adc6d5bb383415f5c20c21bf3ad744945
Luciano Santos <luciano.santos@libercapital.com.br>
2023-03-03 16:32:22 +0000

    feat(find): implement nil return in find

--- slice.go
+++ slice.go
@@ -15,13 +15,13 @@ func FindIndex[T1 any](s []T1, fn func(T1) bool) int {
 	return -1
 }
 
-func Find[T any](s []T, fn func(T) bool) T {
+func Find[T any](s []T, fn func(T) bool) *T {
 	for _, slice := range s {
 		if fn(slice) {
-			return slice
+			return &slice
 		}
 	}
-	return *new(T)
+	return nil
 }
 
 func Map[T any, T2 any](s []T, fn func(T) T2) []T2 {
